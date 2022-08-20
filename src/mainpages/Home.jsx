91db3d55131c5197c4198acec7a266a3d82e5b3a
limdumb <dlaruddls@nate.com>
2022-08-20 13:06:45 +0900

    Diary vol.1

---
+++ src/mainpages/Home.jsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const AppHeader = styled.header`
+    min-height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: calc(10px + 2vmin);
+    color: white;
+    border-bottom: 1px solid rgba(205, 205, 205, 0.3);
+`;
+
+const StartButten = styled.button `
+  background-color: gray
+  height 50px;
+  font-size: 11px;
+  font-weight: bold;
+  padding: 11px;
+  justify-content: flex-end;
+  min-height: 50px;
+  cursor: pointer;
+`
+
+export default function Home(){
+  const [ diaries, setDiaries ] = useState([])
+// 비동기처리 ( 홈이라는 컨포넌트가 마운트(꼽혔을때) 되었을때, 언마운트(뽑혔을때) 될때 실행 )
+  useEffect(() => {
+// setDiaries가 diaries에 값을 할당하기 위해서
+    async function getDiaries() {
+      const res = await axios.get('http://localhost:3004/diaries')
+      setDiaries(res.data)
+    }
+
+    getDiaries();
+  }, [false])
+
+  return(
+  <div>
+    <AppHeader>
+    <a type="Butten" href="/profile">
+      <StartButten>작성하기</StartButten>
+    </a>
+    </AppHeader>
+    <div style={{
+      display: 'flex',
+      flexWrap: 'wrap'
+    }}>
+    {
+      diaries.map(item => {
+        return (
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            
+          }}>
+            {/* react의 핵심개념중 버츄얼 돔 html페이지 받으면 브라우저에 띄우기위해 돔틀구성
+             */}
+          <h3 key={item.createdAt}> {item.title} </h3>
+          <p key={item.title}> {item.createdAt} </p>
+          </div>
+        )
+      })
+    }
+    </div>
+  </div>
+  )
+}
---
+++ src/mainpages/Profile.jsx
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const MyButton = styled.button`
+  background-color: gray
+  height 50px;
+  font-size: 11px;
+  font-weight: bold;
+  padding: 11px;
+  min-height: 50px;
+  cursor: pointer;
+  `;
+
+const AppHeader = styled.header`
+    min-height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: calc(10px + 2vmin);
+    color: white;
+    border-bottom: 1px solid rgba(205, 205, 205, 0.3);
+`;
+
+const SelectTitle = styled.label `
+    font-size: 14px;
+    font-weight: bold;
+`;
+
+const InputTitle = styled.input.attrs({ placeholder: "오늘 나의 하루일기"})`
+    border-left: none;
+    border-right: none;
+    border-top: none;
+    border-bottom-width: 3px;
+    border-bottom-color: black;
+    width: 459px;
+    font-size: 14px;
+    font-weight: bold;
+    justify-content: center;
+`;
+
+const Diarycontent = styled.textarea`
+    margin-left: 30px;
+    position: absolute;
+    padding: 30px 10px;
+    font-size: 10px;
+    width: 400px;
+    line-height: 20px;
+    justify-content: center;
+    border: 1px solid rgba(255, 255, 255, 0.5);
+`
+const EmotionSelect = styled.select`
+    font-size : 14px;
+    font-weight: bold;
+`
+
+export default function Profile() {
+  return <div>
+<AppHeader>
+  <a type='button' href='/'>
+        <MyButton> 뒤로가기 </MyButton>
+  </a>
+  <MyButton onClick={console.log}> 저장하기 </MyButton>
+      </AppHeader>
+      <div>
+        <p style={{
+          display: 'flex',
+          'justify-content': 'center',
+        }}>
+        <SelectTitle> 제목: </SelectTitle> <InputTitle></InputTitle>
+          <EmotionSelect>
+            <option key="Good" value="Good">좋음</option>
+            <option key="Soso" value="Soso">보통</option>
+            <option key="Bad" value="Bad">나쁨</option>
+          </EmotionSelect>
+        </p>
+          <div style={{
+                display: 'flex',
+                'justify-content': 'center',
+          }}>
+            <Diarycontent></Diarycontent>
+          </div>
+      </div>
+    </div>;
+}
