28d52dccee5c0986723dab302ee14bb851d82d29
limdumb <dlaruddls@nate.com>
2022-08-29 09:28:54 +0900

    감정상태 선택기능 추가

---
+++ src/Components/MyEmotion.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+import SmileImage from './Happy.png'
+import React, { useState } from 'react';
+import SosoImage from './Soso.png'
+import BadImage from './Bad.png'
+
+
+export function MyEmotion() {
+  const [emotion, setEmotion] = useState(false);
+  const [sosoImage, setSosoImage] = useState(false);
+  const [smileImage, setSmileImage] = useState(true);
+  const [badImage, setBadImage] = useState(false);
+
+  const onOffToggle = (() => {
+    setEmotion(!emotion)
+  })
+
+  const changeEmotionImage = () => {
+    setSosoImage = () => {
+      onclick()
+    }
+  }
+  
+
+  const renderEmotionImage = () => {
+    if (emotion === true) {
+      return (
+        <div>
+          <img src={SmileImage} width={40} height={40} onClick={() => { setSmileImage(true); setSosoImage(false); setBadImage(false); setEmotion(false); }}/>
+          <img src={SosoImage} width={40} height={40} onClick={() => { setSmileImage(false); setSosoImage(true); setBadImage(false); setEmotion(false); }}/>
+          <img src={BadImage} width={40} height={40} onClick={() => { setSmileImage(false); setSosoImage(false); setBadImage(true); setEmotion(false); }}/>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
+
+  return (
+    <div>
+        { sosoImage && <img src={SosoImage} width={40} height={40} onClick={onOffToggle}/>}
+        { smileImage && <img src={SmileImage} width={40} height={40} onClick={onOffToggle}/>}
+        { badImage && <img src={BadImage} width={40} height={40} onClick={onOffToggle}/>}
+        
+        {renderEmotionImage()}        
+    </div>
+  )
+}
